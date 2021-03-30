2c2467a3b1817599cd35117d1ebfa58ad3cf426f
langan.kg Langan <langan.kg@gmail.com>
2021-03-30 19:32:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,9 +30,7 @@ end
 def monopoly_with_third_tier
 empty_hash = {}
 	monopoly = {
-		:railroads => {pieces: 4},
-		:names => {:reading_railroad=>{}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{}},
-		:rent_in_dollars => {:one_piece_owned=>25, :two_pieces_owned=>50, :three_pieces_owned=>100, :four_pieces_owned=>200,}
+		:railroads => {pieces: 4, :names => {:reading_railroad=>{}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{}, :rent_in_dollars => {:one_piece_owned=>25, :two_pieces_owned=>50, :three_pieces_owned=>100, :four_pieces_owned=>200,}
 
 	}
 end
