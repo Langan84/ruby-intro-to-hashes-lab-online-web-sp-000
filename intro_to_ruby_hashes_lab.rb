ec0bc3f75bf2a2a5045b20e898444b5a23548452
langan.kg Langan <langan.kg@gmail.com>
2021-03-30 19:50:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,8 +40,8 @@ def monopoly_with_third_tier
 			:pennsylvania_railroad=>{},
 			:b_and_o_railroad=>{},
 			:shortline=>{},
-			:reading_railroad=>
-       
+			:reading_railroad=>{}
+
      }
    }
  }
